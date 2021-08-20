508e0ef9bd184752ad5b0fbdb9585b73ee29dbe0
dennaz2 <dennabellman@gmail.com>
2021-08-19 23:58:30 -0400

    first commit

---
+++ src/App.java
@@ -0,0 +1,24 @@
+public class App {
+    //Book class declaration 
+    public class Book{
+        public Book(int bookId, String bookName, String writerName, double price, int quantity){
+            
+        }
+
+        public static void addBookDetails(){
+
+        }
+        
+        public static void deleteBookDetails(){
+            
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //Colllection of books to store the book
+        //an arrayList Of type book , that stores instances of books 
+        Book[] books;
+
+
+    }
+}
