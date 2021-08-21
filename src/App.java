08fecffbfb352720b46b00b588ba4de276e52a78
Denna Bellman <46805312+dennaz2@users.noreply.github.com>
2021-08-20 23:10:58 -0400

    initialized fieleds and generated unqioe book id

--- src/App.java
+++ src/App.java
@@ -1,9 +1,13 @@
+import java.util.Random;
+import java.util.UUID;
+
 public class App {
-    //Book class declaration 
+    //Create a class Book, for addition of book details.
     public class Book{
         //Add required fields such as bookId, bookName, writerName, price and quantity.
-        int bookId, quantity;
-        String bookName, writerName;
+        int quantity;
+        Random randNumber;
+        String bookName, writerName, bookId;
         double price;
 
 
@@ -11,9 +15,27 @@ public class App {
             //Colllection of books to store the book
             //an arrayList Of type book , that stores instances of books 
             Book[] books;
+
+            //Make example book to see if thisd works
+
+            //Add book to book list 
+            
     
         }
 
+        //Constructor to accept details for a specific book and store the same in an object of type Book.
+        public Book(String bookName, String writerName, double price, int quantity){
+            //insitialize fields of object 
+            this.bookName = bookName;
+            this.writerName = writerName;
+            this.price = price;
+            this.quantity = quantity;
+
+            //Every time you add the details of a new book in your already present collection, generate a unique bookId for the same.
+            String uniqueID = UUID.randomUUID().toString();
+            this.bookId = uniqueID;
+
+        }
 
     }
 
